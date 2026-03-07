932afae659b97702336c215dc530462b65b5273d
Peter Keintzel <p.keintzel@live.at>
2026-03-07 08:07:45 +0100

    [GUI] Responsive UI update

--- Z2X-Programmer/Converter/DoubleByteConverter.cs
+++ Z2X-Programmer/Converter/DoubleByteConverter.cs
@@ -1,19 +1,40 @@
-using System;
+/*
+
+Z2X-Programmer
+Copyright (C) 2024 - 2026
+PeterK78
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see:
+
+https://github.com/PeterK78/Z2X-Programmer?tab=GPL-3.0-1-ov-file.
+
+*/
+
 using System.Globalization;
-using Microsoft.Maui.Controls;
 
 namespace Z2XProgrammer.Converter
 {
     public class DoubleByteConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             if (value == null) return 0.0;
             try { return System.Convert.ToDouble(value, culture); }
             catch { return 0.0; }
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             if (value == null) return (byte)0;
             double d;
